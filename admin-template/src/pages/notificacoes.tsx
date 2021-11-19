9564b6261e10008d4cece6dc53f64e5af68039aa
edgarbispo <edgarbispo@hotmail.com>
2021-11-19 07:20:28 -0300

    Admin-Template

--- admin-template/src/pages/notificacoes.tsx
+++ admin-template/src/pages/notificacoes.tsx
@@ -1,10 +1,26 @@
 import Layout from "../components/template/Layout";
+//import {AppConsumer} from "../data/context/AppContext";
+import useAppData from "../data/hook/useAppData";
 
 export default function Notificacoes() {
+
+    const ctx = useAppData()
+
     return (
         <Layout titulo={"Notificações"}
                 subtitulo={"Aqui você irá gerenciar as suas notificações"}>
-            <h3>Conteúdo!!!</h3>
+
+            {/* Nesse caso o AppConsumer precisa ser definido
+                no arquivo AppContext.tsx
+             ==>> Substituido pelo useAppData
+
+            <AppConsumer>
+                {dados => <h3>{dados.nome}</h3>}
+            </AppConsumer>
+
+            */}
+
+            <h3>{ctx.nome}</h3>
         </Layout>
     )
 }
