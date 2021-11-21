d81ab36a6feaf98c73ce05ef49824b3218376b37
edgarbispo <edgarbispo@hotmail.com>
2021-11-21 13:01:38 -0300

    Formulário - Novo Registro

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -3,6 +3,7 @@ import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
 import Botao from "../components/Botao";
 import Formulario from "../components/Formulario";
+import {useState} from "react";
 
 export default function Home() {
 
@@ -21,23 +22,39 @@ export default function Home() {
         console.log(cliente.nome)
     }
 
+    function salvarCliente(cliente: Cliente) {
+        console.log(cliente)
+    }
+
+    const [visibel, setVisivel] = useState<'tabela' | 'form'>('tabela')
+
   return (
     <div className={`flex justify-center items-center h-screen
     bg-gradient-to-r from-blue-500 to-purple-500
     text-white
     `}>
         <Layout titulo={"Cadastro Simples"}>
-            <div className={`flex justify-end`}>
-                <Botao cor={"green"}
-                    className={"mb-4"}>Novo Cliente</Botao>
-            </div>
-{/*
-            <Tabela clientes={clientes}
-                    clienteSelecionado={clienteSelecionado}
-                    clienteExcluido={clienteExcluido}
-            />
-*/}
-            <Formulario cliente={clientes[0]}/>
+            {visibel === 'tabela' ? (
+                <>
+                    <div className={`flex justify-end`}>
+                        <Botao cor={"green"}
+                            className={"mb-4"}
+                            onClick={() => setVisivel('form')}>
+                            Novo Cliente
+                        </Botao>
+                    </div>
+                    <Tabela clientes={clientes}
+                            clienteSelecionado={clienteSelecionado}
+                            clienteExcluido={clienteExcluido}
+                    />
+                </>
+            ) : (
+                <Formulario
+                    cliente={clientes[0]}
+                    clienteMudou={salvarCliente}
+                    cancelado={() => setVisivel('tabela')}
+                />
+            )}
         </Layout>
     </div>
   )
