a3af35e7e139ff67996d48147f090e182f43f634
edgarbispo <edgarbispo@hotmail.com>
2021-11-21 10:31:11 -0300

    Formulário Carregando 1a Cliente

--- next-crud/src/components/Formulario.tsx
+++ next-crud/src/components/Formulario.tsx
@@ -1,13 +1,48 @@
 import Entrada from "./Entrada";
+import {useState} from "react";
+import Cliente from "../core/Cliente";
+import Botao from "./Botao";
 
 interface FormularioProps {
-
+    cliente: Cliente
 }
 
 export default function Formulario(props:FormularioProps) {
+
+    const id = props.cliente?.id
+    const [nome, setNome] = useState(props.cliente?.nome ?? '')
+    const [idade, setIdade] = useState(props.cliente?.idade ?? 0)
+
     return(
         <div>
-            <Entrada texto={"Nome"} valor={"Teste"} />
+            {id ? (
+                <Entrada
+                    somenteLeitura
+                    texto={"Código"}
+                    valor={id}
+                    className={"mb-4"}
+                />
+            ) : false}
+            <Entrada
+                texto={"Nome"}
+                valor={nome}
+                valorMudou={setNome}
+                className={"mb-4"}
+            />
+            <Entrada
+                texto={"Idade"}
+                tipo={"number"}
+                valor={idade}
+                valorMudou={setIdade}
+            />
+            <div className={"flex justify-end mt-7"}>
+                <Botao cor={"blue"} className={"mr-2"}>
+                    {id ? 'Alterar' : 'Salvar'}
+                </Botao>
+                <Botao>
+                    Cancelar
+                </Botao>
+            </div>
         </div>
     )
 }
--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -37,7 +37,7 @@ export default function Home() {
                     clienteExcluido={clienteExcluido}
             />
 */}
-            <Formulario/>
+            <Formulario cliente={clientes[0]}/>
         </Layout>
     </div>
   )
