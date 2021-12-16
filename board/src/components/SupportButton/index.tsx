913e12b1c476923e923e30e984014366791ec073
edgarbispo <edgarbispo@hotmail.com>
2021-12-16 16:12:55 -0300

    42. Botão seja apoiador

---
+++ board/src/components/SupportButton/index.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import styles from './styles.module.scss'
+export function SupportButton(){
+    return(
+        <div className={styles.doanteContainer}>
+            <Link href={"/donate"}>
+                <button>
+                    Apoiar
+                </button>
+            </Link>
+        </div>
+    )
+}
