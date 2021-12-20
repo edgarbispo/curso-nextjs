28ecff0b56964ba5a1d839f1c72e626f56b64e30
edgarbispo <edgarbispo@hotmail.com>
2021-12-20 17:18:23 -0300

    53. Página detalhes pt2

--- board/src/pages/board/[id].tsx
+++ board/src/pages/board/[id].tsx
@@ -3,6 +3,10 @@ import {getSession} from "next-auth/client";
 import firebase from '../../services/firebaseConnection'
 import {format} from 'date-fns'
 
+import Head from 'next/head'
+import styles from './task.module.scss'
+import {FiCalendar} from 'react-icons/fi'
+
 type Task = {
     id: string,
     created: string | Date,
@@ -22,10 +26,21 @@ export default function Task({data}: TaskListProps) {
     const task = JSON.parse(data) as Task
 
     return(
-        <div>
-            <h1>Página Detalhes</h1>
-            <h2>{task.tarefa}</h2>
-        </div>
+        <>
+            <Head>
+                <title>Detalhes da sua tarefa</title>
+            </Head>
+            <article className={styles.container}>
+                <div className={styles.actions}>
+                    <div>
+                        <FiCalendar size={30} color={"#FFF"}/>
+                        <span>Tarefa criada:</span>
+                        <time>{task.createdFormatted}</time>
+                    </div>
+                </div>
+                <p>{task.tarefa}</p>
+            </article>
+        </>
     )
 }
 
