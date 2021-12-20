08003d1487f98fe46de49ba0296903754ebd6afb
edgarbispo <edgarbispo@hotmail.com>
2021-12-20 19:28:08 -0300

    57. Configurando pagamento pt2
    Pay Pal
    https://developer.paypal.com/docs/checkout/reference/customize-sdk/
    
    https://developer.paypal.com/developer/accounts?event=createSuccess
    
    npm install @paypal/react-paypal-js

--- board/src/pages/donate/index.tsx
+++ board/src/pages/donate/index.tsx
@@ -3,9 +3,15 @@ import styles from './styles.module.scss'
 import Head from 'next/head';
 import {GetServerSideProps} from "next";
 import {getSession} from "next-auth/client";
+import firebase from '../../services/firebaseConnection'
+
+import {PayPalButtons} from "@paypal/react-paypal-js";
+import {useState} from "react";
 
 // Client ID: AcIdRjSLKEIz4FWp8eeHpxTpK5AJ_pBpB7Y_FFuDj2C4AMJO27Fs0zExVkd1Mm6ni-QwFRE8XiXJJdaL
-//<script src="https://www.paypal.com/sdk/js?client-id=YOUR_CLIENT_ID"></script>
+//<script src="https://www.npm paypal.com/sdk/js?client-id=YOUR_CLIENT_ID"></script>
+// E-MAIL DE TESTE: sb-vnrnf8594835@business.example.com
+// SENHA: bispo112233
 
 interface DonateProps{
     user: {
@@ -16,6 +22,22 @@ interface DonateProps{
 }
 
 export default function Donate({user}: DonateProps){
+
+    const [vip, setVip] = useState(false);
+
+    async function handleSaveDonate() {
+        await firebase.firestore().collection('users')
+            .doc(user.id)
+            .set({
+                donate: true,
+                lastDonate: new Date(),
+                image: user.image
+            })
+            .then(() => {
+                setVip(true);
+            })
+    }
+
     return(
         <>
             <Head>
@@ -24,14 +46,38 @@ export default function Donate({user}: DonateProps){
             <main className={styles.container}>
                 <img src="/images/rocket.svg" alt="Seja Apoiador"/>
 
-                <div className={styles.vip}>
-                    <img src={user.image} alt="Foto de perfil do usuário"/>
-                    <span>Parabéns você um novo apoiador</span>
-                </div>
+                { vip && (
+                    <div className={styles.vip}>
+                        <img src={user.image} alt="Foto de perfil do usuário"/>
+                        <span>Parabéns você um novo apoiador</span>
+                    </div>
+                )}
 
                 <h1>Seja um apoiador desse projeto! 🏆</h1>
                 <h3>Contribua com apenas <span>R$ 1,00</span> </h3>
                 <strong>Apareça na nossa home, tenha funcionalidades exclusivas</strong>
+
+                <PayPalButtons
+                    createOrder={(data, actions) => {
+                        return actions.order.create({
+                            purchase_units: [{
+                                amount: {
+                                    value: '1'
+                                }
+                            }]
+                        })
+                    }}
+                    onApprove={(data, actions) => {
+                        return actions.order.capture()
+                            .then(function(details){
+                                console.log('Compra aprovada: ' + details.payer.name.given_name);
+                                handleSaveDonate();
+                            })
+                    }}
+                >
+
+                </PayPalButtons>
+
             </main>
         </>
     )
