43ce03c31b858962dc088df2b84b6b8892e96e7e
edgarbispo <edgarbispo@hotmail.com>
2022-01-25 07:47:36 -0300

    84. Criando Paginação no blog

--- sujeito/src/pages/posts/index.tsx
+++ sujeito/src/pages/posts/index.tsx
@@ -25,11 +25,54 @@ type Post ={
 
 interface PostProps{
     posts: Post[];
+    page: string;
+    totalPage: string
 }
 
-export default function Posts({posts: postsBlog}: PostProps){   //Renomeado a propriedade posts para postsBlog
+export default function Posts({posts: postsBlog, page, totalPage}: PostProps){   //Renomeado a propriedade posts para postsBlog
 
-    const [posts, setPosts] = useState(postsBlog || [])
+    const [currentPage, setCurrentPage] = useState(Number(page));
+    const [posts, setPosts] = useState(postsBlog || []);
+
+    //Buscar novos posts
+    async function reqPost(pageNumber: number){
+        const prismic = getPrismicClient();
+        const response = await prismic.query([
+            Prismic.Predicates.at('document.type', 'post')
+        ], {
+            orderings: '[document.last_publication_date desc]', // Ordenar de forma descrecent
+            fetch: ['post.title', 'post.description', 'post.cover'],
+            pageSize: 3,
+            page: String(pageNumber)
+        })
+
+        return response;
+    }
+
+    async function navigatePage(pageNumber: number){
+        const response = await reqPost(pageNumber);
+
+        if (response.results.length === 0) {
+            return;
+        }
+
+        const getPosts = response.results.map(post => {
+            return {
+                slug: post.uid,
+                title: RichText.asText(post.data.title),
+                description: post.data.description.find(content => content.type === 'paragraph')?.text ?? '',
+                cover: post.data.cover.url,
+                updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric'
+                })
+            }
+        })
+
+        setCurrentPage(pageNumber);
+        setPosts(getPosts);
+    }
 
     return(
         <>
@@ -59,23 +102,28 @@ export default function Posts({posts: postsBlog}: PostProps){   //Renomeado a pr
 
 
                     <div className={styles.buttonNavigate}>
-                        <div>
-                            <button>
-                                <FiChevronsLeft size={25} color={"#FFF"}/>
-                            </button>
-                            <button>
-                                <FiChevronLeft size={25} color={"#FFF"}/>
-                            </button>
-                        </div>
+                        { Number(currentPage) >= 2 && (
+                            <div>
+                                <button onClick={() => navigatePage(1)}>
+                                    <FiChevronsLeft size={25} color={"#FFF"}/>
+                                </button>
+                                <button onClick={() => navigatePage(Number(currentPage - 1))}>
+                                    <FiChevronLeft size={25} color={"#FFF"}/>
+                                </button>
+                            </div>
+                        )}
 
-                        <div>
-                            <button>
-                                <FiChevronRight size={25} color={"#FFF"}/>
-                            </button>
-                            <button>
-                                <FiChevronsRight size={25} color={"#FFF"}/>
-                            </button>
-                        </div>
+                        { Number(currentPage) < Number(totalPage) && (
+                            <div>
+                                <button onClick={() => navigatePage(Number(currentPage + 1))}>
+                                    <FiChevronRight size={25} color={"#FFF"}/>
+                                </button>
+                                <button onClick={() => navigatePage(Number(totalPage))}>
+                                    <FiChevronsRight size={25} color={"#FFF"}/>
+                                </button>
+                            </div>
+                        )}
+
                     </div>
                 </div>
             </main>
@@ -110,7 +158,9 @@ export const getStaticProps: GetStaticProps = async () => {
 
     return{
         props: {
-            posts
+            posts,
+            page: response.page,
+            totalPage: response.total_pages
         },
         revalidate: 60 * 30 //Atualiza a cada 30 minutos
     }
